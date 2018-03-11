cc5555e80cf902b77cbc5727625e61e065661530
peper11111 <peper.pleban@gmail.com>
2018-03-11 13:32:10 +0100

    Project init.

---
+++ src/main/java/pl/edu/pw/ee/cookbookserver/CookbookServerApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.pw.ee.cookbookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookbookServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookbookServerApplication.class, args);
+    }
+}
