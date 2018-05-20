5af34f75045a2ac03b23b495877378210cec4457
peper11111 <peper.pleban@gmail.com>
2018-05-20 13:10:49 +0200

    Deleted unnecessary import.

--- src/main/java/pl/edu/pw/ee/cookbookserver/controller/UploadController.java
+++ src/main/java/pl/edu/pw/ee/cookbookserver/controller/UploadController.java
@@ -1,7 +1,6 @@
 package pl.edu.pw.ee.cookbookserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
